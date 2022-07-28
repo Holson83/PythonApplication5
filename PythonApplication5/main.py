f5a10f9720007969abd93c2f39489d985c9be0fc
Иван Тусов <vanyatusov@gmail.com>
2022-07-28 17:44:29 +0300

    I keep changing the code

---
+++ PythonApplication5/main.py
@@ -0,0 +1,3 @@
+from solve_quadratic import solve_quadratic
+
+print(solve_quadratic(3, 2, 0))
--- PythonApplication5/solve_quadratic.py
+++ PythonApplication5/solve_quadratic.py
@@ -1,34 +1,29 @@
 ﻿import math
 
+def solve_linear(a: float, b: float): # a*x + b = 0
+    return -b / a
 
-def solve_quadratic(a: float, b: float, c: float):
-    discr = b ** 2 - 4 * a * c
+def solve_quadratic(a: float, b: float, c: float): # a*x^2 + b*x + c = 0
+    print(f'solve_quadratic({a}, {b}, {c})')
+    try:
+        print('try start')
+        discr = b ** 2 - 4 * a * c
 
-    if discr > 0:
-        x1 = (-b + discr ** 0.5) / (2 * a)
-        x2 = (-b - discr ** 0.5) / (2 * a)
-        return (x1, x2)
-    elif discr == 0:
-        x1 = -b / (2 * a)
-        return (x1, x1)
-    elif a == 0:
-        try:
-            if discr > 0:
-                return None
-            elif discr == 0:
-                return None
-        except ZeroDivisionError:
-            a = 0
-    elif b == 0:
-        try:
-            if discr > 0:
-                x1 = (discr ** 0.5) / (2 * a)
-                x2 = (discr ** 0.5) / (2 * a)
-                return (x1, x2)
-            elif discr == 0:
-                x1 = 0
-                return (x1, x1)
-        except ZeroDivisionError:
-            b = 0
-    else:
+        if discr > 0:
+            x1 = (-b + discr ** 0.5) / (2 * a)
+            x2 = (-b - discr ** 0.5) / (2 * a)
+            return (x1, x2)
+
+        elif discr == 0:
+            x1 = -b / (2 * a)
+            return (x1, x1)
+
+        else:
+            return None
+
+    except ArithmeticError:
+        print('ArithmeticError')
         return None
+
+    finally:
+        print('Finally')
